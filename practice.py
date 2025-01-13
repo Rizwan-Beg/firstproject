ab1d48706387df7c7d0d4c15409ae95dca547200
Rizwan <rizwan@Rizwans-MacBook-Air.local>
2025-01-13 16:30:25 +0530

    again

--- practice.py
+++ practice.py
@@ -67,31 +67,66 @@ print(25*"-")
 # retriever.greet()
 # retriever.sound()
 # Parent Class
+# class Dog:
+#     def sound(self):
+#         print("dog sound")  # Default implementation
+
+# # Run-Time Polymorphism: Method Overriding
+# class Labrador(Dog):
+#     def sound(self):
+#         print("Labrador woofs")  # Overriding parent method
+
+# class Beagle(Dog):
+#     def sound(self):
+#         print("Beagle Barks")  # Overriding parent method
+
+# # Compile-Time Polymorphism: Method Overloading Mimic
+# class Calculator:
+#     def add(self, a, b=0, c=0):
+#         return a + b + c  # Supports multiple ways to call add()
+
+# # Run-Time Polymorphism
+# dogs = [Dog(), Labrador(), Beagle()]
+# for dog in dogs:
+#     dog.sound()  # Calls the appropriate method based on the object type
+
+
+# # Compile-Time Polymorphism (Mimicked using default arguments)
+# calc = Calculator()
+# print(calc.add(5, 10))  # Two arguments
+# print(calc.add(5, 10, 15))  # Three arguments
 class Dog:
-    def sound(self):
-        print("dog sound")  # Default implementation
+    def __init__(self, name, breed, age):
+        self.name = name  # Public attribute
+        self._breed = breed  # Protected attribute
+        self.__age = age  # Private attribute
 
-# Run-Time Polymorphism: Method Overriding
-class Labrador(Dog):
-    def sound(self):
-        print("Labrador woofs")  # Overriding parent method
+    # Public method
+    def get_info(self):
+        return f"Name: {self.name}, Breed: {self._breed}, Age: {self.__age}"
 
-class Beagle(Dog):
-    def sound(self):
-        print("Beagle Barks")  # Overriding parent method
+    # Getter and Setter for private attribute
+    def get_age(self):
+        return self.__age
 
-# Compile-Time Polymorphism: Method Overloading Mimic
-class Calculator:
-    def add(self, a, b=0, c=0):
-        return a + b + c  # Supports multiple ways to call add()
+    def set_age(self, age):
+        if age > 0:
+            self.__age = age
+        else:
+            print("Invalid age!")
 
-# Run-Time Polymorphism
-dogs = [Dog(), Labrador(), Beagle()]
-for dog in dogs:
-    dog.sound()  # Calls the appropriate method based on the object type
+# Example Usage
+dog = Dog("Buddy", "Labrador", 3)
 
+# Accessing public member
+print(dog.name)  # Accessible
 
-# Compile-Time Polymorphism (Mimicked using default arguments)
-calc = Calculator()
-print(calc.add(5, 10))  # Two arguments
-print(calc.add(5, 10, 15))  # Three arguments
+# Accessing protected member
+print(dog._breed)  # Accessible but discouraged outside the class
+
+# Accessing private member using getter
+print(dog.get_age())
+
+# Modifying private member using setter
+dog.set_age(5)
+print(dog.get_info())
